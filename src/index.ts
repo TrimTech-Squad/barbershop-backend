1b1d222dfcd1f98bc03d5cfc6bcc159c7cd4065a
zulfaa <1221130052@global.ac.id>
2023-12-12 08:22:50 +0700

    update routes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 
-import authRoutes from './routes/auth'
+const auth = require ('./routes/auth')
+const kapster = require('./routes/kapsterRoutes')
+const appointment = require('./routes/appointmentRoutes')
+const service = require ('./routes/serviceRoutes')
+const user = require ('./routes/userRoutes')
 
 const app = express()
 
@@ -10,7 +14,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', auth)
+app.use(kapster)
+app.use(appointment)
+app.use(service)
+app.use(user)
 
 app.listen(port, () => {
   console.log('Server running on port 3000')
