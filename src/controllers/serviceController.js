3bfa312fd363de286f19ba99ba86cd5bdb69c938
nishly <adhipamungkaswijayadi@gmail.com>
2023-12-13 19:45:14 +0700

    fixed service controller typing (untested)

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -1,15 +1,23 @@
-const ResponseBuilder = require('../helpers/response-builder');
-const ServiceServices = require('../services/service');
-const ErrorCatcher = require('../helpers/error');
+import ServiceServices from '../services/service'
+import ResponseBuilder from '../helpers/response-builder'
+import ErrorCatcher from '../helpers/error'
 
 //GET DATA
-const getAllService = async (req, res) => {
-   try {
-     const service = await ServiceServices.getAllServices();
-     return ResponseBuilder({ code: 200, data: { service }, message: 'Data Service berhasil diambil' }, res);
-   } catch (error) {
-     return ResponseBuilder(ErrorCatcher(error), res);
-   }
- };
-
-module.exports = {getAllService}
+export const getAllService = async (
+  /** @type {{ body: any; }} */ _req,
+  /** @type {import("express").Response<any, Record<string, any>>} */ res,
+) => {
+  try {
+    const service = await ServiceServices.getAllServices()
+    return ResponseBuilder(
+      {
+        code: 200,
+        data: { service },
+        message: 'Data Service berhasil diambil',
+      },
+      res,
+    )
+  } catch (/** @type {any} */ error) {
+    return ResponseBuilder(ErrorCatcher(error), res)
+  }
+}
